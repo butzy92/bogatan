901aadfa356c0b20afed2f550ada610adc89406c
Marius Butan <Marius.Butan@endava.com>
2017-03-24 18:02:09 +0200

    second commit

--- js/pixie.js
+++ js/pixie.js
@@ -1,32 +1,29 @@
+var scaleStep = 0.1;
+var maxScale = 4;
+var minScale = 0;
+var initScale = 0;
+
 function init() {
+
+    renderer = PIXI.autoDetectRenderer(981, 724, {backgroundColor: 0x34495e});
+    document.body.appendChild(renderer.view);
+
     stage = new PIXI.Container();
-    renderer = PIXI.autoDetectRenderer(
-        981,
-        724,
-        {view: document.getElementById("game-canvas")}
-    );
-    var width = document.getElementById("game-canvas").width;
+
     var farTexture = PIXI.Texture.fromImage("img/atmosphere.png", PIXI.SHAPES.RECT, 1);
-    far = new PIXI.extras.TilingSprite(
-        farTexture,
-        981,
-        724
-    );
-    far.position.x = 0;
-    far.position.y = 0;
-    far.tilePosition.x = 0;
-    far.tilePosition.y = 0;
+    far = new PIXI.Sprite(farTexture);
 
-    farTexture.interactive = true;
-    farTexture.buttonMode = true;
+    far.interactive = true;
+    far.buttonMode = true;
 
+    far.on("mousemove",mouseMove())
+        .on("mousedown", mouseDown())
+        .on("mouseup", mouseUp())
+        .on("pointermove",mouseMove())
+        .on("pointerdown", mouseDown())
+        .on("pointerup", mouseUp());
 
-    farTexture.on('pointerdown', onDragStart)
-        .on('pointerup', onDragEnd)
-        .on('pointerupoutside', onDragEnd)
-        .on('pointermove', onDragMove);
-
-
+    document.addEventListener("mousewheel", wheelDown, false);
     stage.addChild(far);
     // var midTexture = PIXI.Texture.fromImage("img/towers.png");
     // mid = new  PIXI.extras.TilingSprite(
@@ -40,41 +37,107 @@ function init() {
     // mid.tilePosition.y = 0;
     // stage.addChild(mid);
     requestAnimationFrame(update);
+
     renderer.render(stage);
-}
 
-
-function onDragStart(event) {
-    console.log("a intraaaat")
-    // store a reference to the data
-    // the reason for this is because of multitouch
-    // we want to track the movement of this particular touch
-    this.data = event.data;
-    this.alpha = 0.5;
-    this.dragging = true;
-}
-
-function onDragEnd() {
-    this.alpha = 1;
-    this.dragging = false;
-    // set the interaction data to null
-    this.data = null;
-}
-
-function onDragMove() {
-    if (this.dragging) {
-        var newPosition = this.data.getLocalPosition(this.parent);
-        this.tilePosition.x = newPosition.x;
-        this.tilePosition.y = newPosition.y;
+    function mouseMove(){
+        console.log("misc mouse")
     }
-}
+    function mouseDown(event){
+        //this.data = event.data
+        this.dragging = true;
+        console.log("misc jos")
+    }
 
+    function mouseUp(){
+        //this.data = null
+        this.dragging = false
+        console.log("misc sus")
+    }
+
+
+}
 
 function update() {
-  //  far.tilePosition.x -= 0.128;
+    //  far.tilePosition.x -= 0.128;
 //    mid.tilePosition.x -= 0.64;
 
     renderer.render(stage);
 
-   requestAnimationFrame(update);
+    requestAnimationFrame(update);
 }
+
+function wheelDown(e) {
+    var e = window.event || e
+    var up = e.deltaY > 0;
+    console.log("am intrat")
+    console.log(e)
+
+    if ((up && initScale == maxScale) || (!up && initScale == minScale)){
+        return ;
+    }
+
+
+
+    if (up) {
+        stage.scale.x += scaleStep;
+        stage.scale.y += scaleStep;
+        initScale++;
+    } else {
+        stage.scale.x -= scaleStep;
+        stage.scale.y -= scaleStep;
+        initScale--;
+    }
+
+}
+
+
+
+
+function addDragNDrop() {
+    stage.interactive = true;
+
+    var isDragging = false,
+        prevX, prevY;
+
+    stage.mousedown = function (moveData) {
+        var pos = moveData.data.global;
+        //  prevX = pos.x;
+        //   prevY = pos.y;
+        //    isDragging = true;
+    };
+
+    stage.mousemove = function (moveData) {
+        // if (!isDragging) {
+        //     return;
+        // }
+        // var pos = moveData.data.global;
+        // var dx = pos.x - prevX;
+        // var dy = pos.y - prevY;
+
+        console.log("dx=" + dx);
+        console.log("stage.position.x=" + stage.position.x);
+        console.log("far.position.x=" + far.position.x);
+        console.log("pos.x=" + pos.x);
+        console.log("prevX=" + prevX);
+        console.log("dy=" + dy);
+        // if (far.position.x != 0) {
+        //     far.position.x += dx;
+        // }
+        //
+        // if (far.position.y != 0) {
+        //     far.position.y += dy;
+        // }
+
+        //  stage.position.x += dx;
+        // stage.position.y += dy;
+        //     prevX = pos.x;
+        //    prevY = pos.y;
+    };
+
+    stage.mouseup = function (moveDate) {
+        isDragging = false;
+    };
+}
+
+ 
